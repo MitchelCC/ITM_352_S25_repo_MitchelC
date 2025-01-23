958a0e7f77f63e550424e7e753729e7012003f2a
MitchelCC <mitchela@hawaii.edu>
2025-01-23 13:32:13 -1000

    Trying and practicing code

---
+++ Lab Exp/ex1.py
@@ -0,0 +1,8 @@
+input = int(input("Input a number between 1-100: "))  
+number = int(input)
+Squared = number * number 
+
+print(f"You entered: {number}")
+print(f"The square of {number} is {Squared}")
+
+
