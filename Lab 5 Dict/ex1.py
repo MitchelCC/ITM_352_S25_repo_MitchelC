e25459975a8751892a8ad9249eb24ab879c0ae64
MitchelCC <mitchela@hawaii.edu>
2025-02-13 13:16:38 -1000

    lab 5

---
+++ Lab 5 Dict/ex1.py
@@ -0,0 +1,8 @@
+Trip_data = {
+    "Trip_id" : "da7a62fce",
+    "Trip_seconds" : 360,
+    "Trip_miles" : 1.1,
+    "Fare ": "$6.25"
+}
+print(Trip_data)
+print(f"Trip miles are", {Trip_data["Trip_miles"]})
