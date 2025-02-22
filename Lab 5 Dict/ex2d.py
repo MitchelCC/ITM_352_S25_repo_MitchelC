d8ffccb004d38ed45bd4b9e98858efbd7851d259
MitchelCC <mitchela@hawaii.edu>
2025-02-22 11:16:38 -1000

    lab 5 & 6

--- Lab 5 Dict/ex2d.py
+++ Lab 5 Dict/ex2d.py
@@ -1,3 +1,3 @@
-trip_fare = [(1.1, 6.25), (0.8, 5.25), (2.5, 10.50), (2.6, 8.05)]
+trip_fare = [{'duration':1.1 'fare': 6.25}, (1.1, 6.25), (0.8, 5.25), (2.5, 10.50), (2.6, 8.05)]
 print(f"Duration: {trip_fare[2][0]} hours")
 print(f"Cost: ${trip_fare[2][1]}")
---
+++ Lab 5 Dict/ex3.py
@@ -0,0 +1,5 @@
+survey_responses = [5, 7, 3, 8] 
+respondent_ids = (1012, 1035, 1021, 1053)
+
+responses = dict(zip (respondent_ids, survey_responses))
+print(responses)
